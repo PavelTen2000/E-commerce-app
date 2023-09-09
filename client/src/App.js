736542573dc874bb0f7fcddeca70d7c228a0584f
Pavel <paveltenfs@gmail.com>
2023-09-10 03:09:39 +0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import Home from './scenes/Home/Home';
+import ItemDetails from './scenes/ItemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
+import CartMenu from './scenes/global/CartMenu';
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+};
+
+function App() {
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Navbar />
+                <CartMenu></CartMenu>
+                <ScrollToTop />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route
+                        path="item/:itemId"
+                        element={<ItemDetails />}
+                    ></Route>
+                    <Route path="checkout" element={<Checkout />} />
+                    <Route path="checkout/success" element={<Confirmation />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
