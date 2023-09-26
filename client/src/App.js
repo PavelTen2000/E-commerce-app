ceb21af39a2cffbd762060f8a8928c94a8c93413
Pavel <paveltenfs@gmail.com>
2023-09-26 05:05:55 +0500

    added footer component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Checkout from './scenes/checkout/Checkout';
 import Confirmation from './scenes/checkout/Confirmation';
 import Navbar from './scenes/global/Navbar';
 import CartMenu from './scenes/global/CartMenu';
+import Footer from './scenes/global/Footer';
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
@@ -21,7 +22,7 @@ function App() {
         <div className="app">
             <BrowserRouter>
                 <Navbar />
-                <CartMenu></CartMenu>
+                <CartMenu />
                 <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
@@ -32,7 +33,8 @@ function App() {
                     <Route path="checkout" element={<Checkout />} />
                     <Route path="checkout/success" element={<Confirmation />} />
                 </Routes>
-                <CartMenu></CartMenu>
+                <CartMenu />
+                <Footer />
             </BrowserRouter>
         </div>
     );
